01e01cada266954e0555fad42362512604a9c09a
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-06-22 16:20:59 +0530

    polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar home_bar 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "Bars launched..."
